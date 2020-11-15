2a02fa77a761ac7700939f39530ed038f647cd05
Kartikay Sharma <sharma.kartik2107@gmail.com>
2020-11-15 16:29:16 +0530

    Revert "MyProfile fragment implemented. Looking Forward to work with you again."

--- app/src/main/java/com/example/fuelisticv2client/fuelisticv2client/UI/ui/Profile/ProfileViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.fuelisticv2client.fuelisticv2client.UI.ui.Profile;
-
-import androidx.lifecycle.ViewModel;
-
-public class ProfileViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
