016b3d84c0a591626ceac9fca475699d839a8c81
kartikaysharma01 <sharma.kartik2107@gmail.com>
2020-11-22 03:05:31 +0530

    Order Details added

--- app/src/main/java/com/example/fuelisticv2client/fuelisticv2client/Adapter/MyOrdersAdapter.java
+++ app/src/main/java/com/example/fuelisticv2client/fuelisticv2client/Adapter/MyOrdersAdapter.java
@@ -1,17 +1,24 @@
 package com.example.fuelisticv2client.fuelisticv2client.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fuelisticv2client.R;
+import com.example.fuelisticv2client.fuelisticv2client.Callback.IRecyclerClickListener;
 import com.example.fuelisticv2client.fuelisticv2client.Common.Common;
 import com.example.fuelisticv2client.fuelisticv2client.Model.OrderModel;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -62,6 +69,56 @@ public class MyOrdersAdapter extends RecyclerView.Adapter<MyOrdersAdapter.MyView
         holder.txt_delivery_date.setText(new StringBuilder("Delivery date: ").append(orderList.get(position).getDeliveryDate()));
         holder.txt_order_amount.setText(new StringBuilder("Amount: Rs ").append(orderList.get(position).getTotalPayment()));
         holder.txt_order_status.setText(new StringBuilder("Status: ").append(Common.converStatusToText(orderList.get(position).getOrderStatus())));
+
+        holder.setRecyclerClickListener((view, position1) -> showDialog(position1));
+
+    }
+
+    private void showDialog(int position) {
+        View layout_dialog = LayoutInflater.from(context).inflate(R.layout.layout_dialog_order_detail, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setView(layout_dialog);
+
+        Button btn_ok = (Button) layout_dialog.findViewById(R.id.btn_ok);
+        TextInputEditText order_details_order_num= (TextInputEditText) layout_dialog.findViewById(R.id.order_details_order_num);
+        TextInputEditText order_details_order_status= (TextInputEditText) layout_dialog.findViewById(R.id.order_details_order_status);
+        TextInputEditText order_details_transaction_id= (TextInputEditText) layout_dialog.findViewById(R.id.order_details_transaction_id);
+        TextInputEditText order_details_delivery_address= (TextInputEditText) layout_dialog.findViewById(R.id.order_details_delivery_address);
+        TextInputEditText order_details_delivery_charge= (TextInputEditText) layout_dialog.findViewById(R.id.order_details_delivery_charge);
+        TextInputEditText order_details_total_order_amt= (TextInputEditText) layout_dialog.findViewById(R.id.order_details_total_order_amt);
+        TextInputEditText order_details_order_date= (TextInputEditText) layout_dialog.findViewById(R.id.order_details_order_date);
+        TextInputEditText order_details_order_quantity= (TextInputEditText) layout_dialog.findViewById(R.id.order_details_order_quantity);
+        TextInputEditText order_details_delivery_mode= (TextInputEditText) layout_dialog.findViewById(R.id.order_details_delivery_mode);
+        TextInputEditText order_details_delivery_cmt= (TextInputEditText) layout_dialog.findViewById(R.id.order_details_delivery_cmt);
+        TextInputEditText order_details_delivery_date= (TextInputEditText) layout_dialog.findViewById(R.id.order_details_delivery_date);
+
+        order_details_order_num.setText(orderList.get(position).getOrderNumber());
+        order_details_order_status.setText(Common.converStatusToText(orderList.get(position).getOrderStatus()) );
+        order_details_transaction_id.setText(orderList.get(position).getTransactionId());
+        order_details_delivery_address.setText(orderList.get(position).getShippingAddress());
+        order_details_delivery_charge.setText(Integer.toString(orderList.get(position).getDeliveryCharge()));
+
+        calender.setTimeInMillis(orderList.get(position).getOrderDate());
+        Date date = new Date(orderList.get(position).getOrderDate());
+        order_details_order_date.setText(new StringBuilder(Common.getDayOfWeek(calender.get(Calendar.DAY_OF_WEEK)))
+                .append(", ")
+                .append(simpleDateFormat.format(date)));
+        order_details_total_order_amt.setText(String.valueOf(orderList.get(position).getTotalPayment()));
+        order_details_order_quantity.setText(orderList.get(position).getQuantity());
+        order_details_delivery_mode.setText(orderList.get(position).getDeliveryMode());
+        order_details_delivery_cmt.setText(orderList.get(position).getComment());
+        order_details_delivery_date.setText(orderList.get(position).getDeliveryDate());
+
+
+        //show dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        // custom dialog
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().setGravity(Gravity.CENTER);
+
+        btn_ok.setOnClickListener(view -> dialog.dismiss());
     }
 
     @Override
@@ -70,7 +127,7 @@ public class MyOrdersAdapter extends RecyclerView.Adapter<MyOrdersAdapter.MyView
 
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         //        TextView myOrderMenuIcon = findViewById(R.id.txt_order_comment);
         @BindView(R.id.txt_order_amount)
         TextView txt_order_amount;
@@ -86,9 +143,24 @@ public class MyOrdersAdapter extends RecyclerView.Adapter<MyOrdersAdapter.MyView
 
         Unbinder unbinder;
 
+        IRecyclerClickListener recyclerClickListener;
+
+        public void setRecyclerClickListener(IRecyclerClickListener recyclerClickListener) {
+            this.recyclerClickListener = recyclerClickListener;
+        }
+
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             unbinder = ButterKnife.bind(this,itemView);
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            recyclerClickListener.onItemClickListener(view, getAdapterPosition());
+
         }
     }
 
