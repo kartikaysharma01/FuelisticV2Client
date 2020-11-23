b41144aa702c9a7bc0828c6206ca05e50e114383
Kartikay Sharma <sharma.kartik2107@gmail.com>
2020-11-23 23:27:52 +0530

    Update MyFCMServices.java

--- app/src/main/java/com/example/fuelisticv2client/fuelisticv2client/Services/MyFCMServices.java
+++ app/src/main/java/com/example/fuelisticv2client/fuelisticv2client/Services/MyFCMServices.java
@@ -12,11 +12,25 @@ import java.util.Random;
 public class MyFCMServices extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-        Map<String,String> dataRecv = remoteMessage.getData();
-        Common.showNotification(this, new Random().nextInt(),
-                dataRecv.get(Common.NOTI_TITLE),
-                dataRecv.get(Common.NOTI_CONTENT),
-                null);
+      Map<String,String> dataRecv = remoteMessage.getData();
+        if(dataRecv != null)
+        {
+            if(dataRecv.get(Common.NOTI_TITLE).equals("New Order"))
+            {
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra(Common.IS_OPEN_ACTIVITY_NEW_ORDER, true);
+                Common.showNotification(this, new Random().nextInt(),
+                        dataRecv.get(Common.NOTI_TITLE),
+                        dataRecv.get(Common.NOTI_CONTENT),
+                        intent);
+
+            }
+            else
+                Common.showNotification(this, new Random().nextInt(),
+                    dataRecv.get(Common.NOTI_TITLE),
+                    dataRecv.get(Common.NOTI_CONTENT),
+                    null);
+        }
     }
 
     @Override
